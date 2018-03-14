cb4c9b6f8a01c60ba1ba662e17aebb85a31038b8
KebryanSG <KebryanSG@192.168.1.105>
2018-03-14 00:13:02 -0500

    Style SelectPicker Inline

--- mvc/views/Autorizacion/BodegaTipoMovimiento.php
+++ mvc/views/Autorizacion/BodegaTipoMovimiento.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
-<style type="text/css">
-    
-</style>
-
 
 <div class="row" id="Listado">
     <div class="col-md-12">
         <form id="toolbar" class="form-inline">
-
             <div class="form-group ">
                 <label for="" class="control-label">Ciudad</label>
                 <select name="idciudad" class="form-control selectpicker"  required></select>
-
-
             </div>
 
 
